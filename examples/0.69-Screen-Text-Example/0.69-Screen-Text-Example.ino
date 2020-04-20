bd8f58f0b8da43be49d8b992121e5d2c7140b683
Rena <info@tinycircuits.com>
2020-04-20 18:15:28 -0400

    more examples

--- examples/0.69-Screen-Text-Example/0.69-Screen-Text-Example.ino
+++ examples/0.69-Screen-Text-Example/0.69-Screen-Text-Example.ino
@@ -4,15 +4,10 @@
 #include <TinierScreen.h>
 #include <GraphicsBuffer.h>
 
-//TinierScreen display = TinierScreen(TinierScreen042);
 TinierScreen display = TinierScreen(TinierScreen069);
-//TinierScreen display = TinierScreen(TinierScreen096);
-
-//GraphicsBuffer screenBuffer = GraphicsBuffer(72, 40, colorDepth1BPP);
 GraphicsBuffer screenBuffer = GraphicsBuffer(96, 16, colorDepth1BPP);
-//GraphicsBuffer screenBuffer = GraphicsBuffer(128, 64, colorDepth1BPP);
 
-int displayPort = 3;
+int displayPort = 0;
 int resetPin = A0+displayPort;
 
 void setup() {
@@ -34,7 +29,7 @@ void setup() {
 int increment = 0;
 int xMax, yMax, x, y;
 void loop() {
-  xMax = screenBuffer.width + 20 - screenBuffer.getPrintWidth("KEEP WASHING!");
+  xMax = screenBuffer.width + 20 - screenBuffer.getPrintWidth("Text Test!");
   yMax = screenBuffer.height + 8 - screenBuffer.getFontHeight();
   x = increment % xMax; if ((increment / xMax) & 1) x = xMax - x;
   y = increment % yMax; if ((increment / yMax) & 1) y = yMax - y;
@@ -44,7 +39,7 @@ void loop() {
   Wireling.selectPort(displayPort);
   screenBuffer.clear();
   screenBuffer.setCursor(x, y);
-  screenBuffer.print("KEEP WASHING!");
+  screenBuffer.print("Text Test!");
   Wire.setClock(1000000);
   display.writeBuffer(screenBuffer.getBuffer(), screenBuffer.getBufferSize());
   Wire.setClock(50000);
--- examples/0.96-Screen-Text-Example/0.96-Screen-Text-Example.ino
+++ examples/0.96-Screen-Text-Example/0.96-Screen-Text-Example.ino
@@ -5,14 +5,14 @@
 #include <GraphicsBuffer.h>
 
 //TinierScreen display = TinierScreen(TinierScreen042);
-TinierScreen display = TinierScreen(TinierScreen069);
-//TinierScreen display = TinierScreen(TinierScreen096);
+//TinierScreen display = TinierScreen(TinierScreen069);
+TinierScreen display = TinierScreen(TinierScreen096);
 
 //GraphicsBuffer screenBuffer = GraphicsBuffer(72, 40, colorDepth1BPP);
-GraphicsBuffer screenBuffer = GraphicsBuffer(96, 16, colorDepth1BPP);
-//GraphicsBuffer screenBuffer = GraphicsBuffer(128, 64, colorDepth1BPP);
+//GraphicsBuffer screenBuffer = GraphicsBuffer(96, 16, colorDepth1BPP);
+GraphicsBuffer screenBuffer = GraphicsBuffer(128, 64, colorDepth1BPP);
 
-int displayPort = 3;
+int displayPort = 0;
 int resetPin = A0+displayPort;
 
 void setup() {
@@ -34,7 +34,7 @@ void setup() {
 int increment = 0;
 int xMax, yMax, x, y;
 void loop() {
-  xMax = screenBuffer.width + 20 - screenBuffer.getPrintWidth("KEEP WASHING!");
+  xMax = screenBuffer.width + 20 - screenBuffer.getPrintWidth("Text Test!");
   yMax = screenBuffer.height + 8 - screenBuffer.getFontHeight();
   x = increment % xMax; if ((increment / xMax) & 1) x = xMax - x;
   y = increment % yMax; if ((increment / yMax) & 1) y = yMax - y;
@@ -44,7 +44,7 @@ void loop() {
   Wireling.selectPort(displayPort);
   screenBuffer.clear();
   screenBuffer.setCursor(x, y);
-  screenBuffer.print("KEEP WASHING!");
+  screenBuffer.print("Text Test!");
   Wire.setClock(1000000);
   display.writeBuffer(screenBuffer.getBuffer(), screenBuffer.getBufferSize());
   Wire.setClock(50000);
